46cb4635013b9944b8ce412c764999564253a0bd
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-06 18:42:25 +0530

    Adding Firewalls

---
+++ modules/firewall/main.tf
@@ -0,0 +1,12 @@
+resource "google_compute_firewall" "allow_iap_ssh" {
+  name    = "${var.network_name}-allow-iap-ssh"
+  network = var.network_name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  # This is the magic IP range. It is Google's internal IAP proxy.
+  source_ranges = ["35.235.240.0/20"]
+}
