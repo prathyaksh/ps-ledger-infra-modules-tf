9a48c3ab5db4e23bd8c851cffa55544bc2be5edd
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-15 02:07:09 +0530

    upatinf monitoring op

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -1,7 +1,7 @@
 output "dashboard_id" {
-  value = google_monitoring_dashboard.this.id
+  value = google_monitoring_dashboard.monitoring_dashboard.id
 }
 
 output "uptime_check_id" {
-  value = google_monitoring_uptime_check_config.https.uptime_check_id
+  value = google_monitoring_uptime_check_config.uptime_https.uptime_check_id
 }
