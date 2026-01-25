c3bc6a37881a3df66a30568af44251972a54adf4
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-25 23:31:29 +0530

    updating mig  outputs.tf

--- modules/mig-compute/outputs.tf
+++ modules/mig-compute/outputs.tf
@@ -1,3 +1,3 @@
 output "instance_group_self_link" {
-    value = google_compute_instance_group.mig_compute_group.self_link  
+    value = google_compute_instance_group.google_compute_region_instance_group_manager.self_link  
 }
