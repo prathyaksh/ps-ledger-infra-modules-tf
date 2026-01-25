158bb10c2cc99419c95040e6c2e34c5a906c408b
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-25 23:43:31 +0530

    modifying oP-3

--- modules/mig-compute/outputs.tf
+++ modules/mig-compute/outputs.tf
@@ -1,3 +1,3 @@
 output "instance_group_self_link" {
-    value = google_compute_instance_group.google_compute_region_instance_group_manager.instance_group
+    value = google_compute_region_instance_group_manager.mig_compute_group.instance_group
 }
