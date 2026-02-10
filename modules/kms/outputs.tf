2181f25183d5428233a9e8728b3323ddf3cfe600
prathyaksh <prathyaksh.ps@gmail.com>
2026-02-10 12:08:52 +0530

    adding outputs

--- modules/kms/outputs.tf
+++ modules/kms/outputs.tf
@@ -1,4 +1,8 @@
 output "key_id" {
     value = google_kms_crypto_key.db_key.id
     }
-    
+
+output "db_password_secret_version_id" {
+  value       = google_secret_manager_secret.db_password.id
+  description = "The full ID of the secret container"
+}
