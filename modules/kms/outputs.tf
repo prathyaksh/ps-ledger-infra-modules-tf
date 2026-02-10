01e380dcded065803af497686ff5744adbd29800
prathyaksh <prathyaksh.ps@gmail.com>
2026-02-10 12:19:06 +0530

    updating kms output

--- modules/kms/outputs.tf
+++ modules/kms/outputs.tf
@@ -2,7 +2,9 @@ output "key_id" {
     value = google_kms_crypto_key.db_key.id
     }
 
+/*
 output "db_password_secret_version_id" {
   value       = google_secret_manager_secret.db_password.id
   description = "The full ID of the secret container"
 }
+*/
